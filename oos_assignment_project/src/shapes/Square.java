4a8b940366e35e97b768219bbe39bf0c38c8b70a
user <user@user-PC>
2021-04-14 20:27:38 +0530

    changes from contributor 2 from contri2_topic_branch_1

--- oos_assignment_project/src/shapes/Square.java
+++ oos_assignment_project/src/shapes/Square.java
@@ -1,5 +1,10 @@
 package shapes;
 
 public class Square {
-
+	public void perimeter()
+	/* this is contribution by contributor 2, topic branch1*/
+	{
+	double peri=2* (4 + 4);
+	System.out.println("Perimeter is: "+ peri);
+	}
 }
