bfdd90eb65d4731b83c1f50c98b69770cba3141f
user <user@user-PC>
2021-04-15 11:46:23 +0530

    commit by contributor 3 topic branch 1

--- oos_assignment_project/src/shapes/Triangle.java
+++ oos_assignment_project/src/shapes/Triangle.java
@@ -1,5 +1,12 @@
 package shapes;
 
 public class Triangle {
+/* code added by contributor 3 topic branch 18*/
 
+public void surfaceArea()
+{
+double area=(8 * 7);
+System.out.println("Surface Area is :"+area);
+}
+	
 }
